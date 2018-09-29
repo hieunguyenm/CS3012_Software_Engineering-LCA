52c62fe32051d87452f8d9cd1c1467a6143a19f7
Conor Gildea <gildeaco@tcd.ie>
2018-09-29 23:59:27 +0100

    Adding LCA tests

--- lca/lca_test.go
+++ lca/lca_test.go
@@ -9,3 +9,36 @@ func Test_Hello(t *testing.T) {
     t.Error("Expected \"Hello\", got ", v)
   }
 }
+
+func Test_LCA_Root_Is_LCA(t *testing.T) {
+	tree:= &node{
+		value:4
+	}
+	tree = insertFromRoot(tree,1)
+    result = findLCA(tree,1)
+	if result != 4 {
+    t.Error("Expected \"4\", got ", v)
+  }
+}
+
+func Test_LCA_Node_Not_Exist(t *testing.T) {
+	tree:= &node{
+		value:4
+	}
+    result = findLCA(tree,1)
+	if result != nil {
+    t.Error("Expected \"nil\" as node with value 1 doesn't exist got ", v)
+  }
+}
+
+func Test_LCA_Good_Behvaiour_Case(t *testing.T) {
+	tree:= &node{
+		value:4
+	}
+	tree = insertFromRoot(tree,1)
+    tree = insertFromRoot(tree,2)
+	result = findLCA(1,2)
+	if result != 3 {
+    t.Error("Expected \"4\", got ", v)
+  }
+}
