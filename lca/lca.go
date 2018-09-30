a2d6d2348a5ed0c2c068a6ca8fa67a4c7bb86dc9
Conor Gildea <gildeaco@tcd.ie>
2018-09-30 23:25:53 +0100

    Added findPath() and findLCA() function

--- lca/lca.go
+++ lca/lca.go
@@ -4,7 +4,8 @@ package lca
 
 // First Golang project and CI project
 // Used golang.org/x/tour/tree as a basis for insert function
-
+// Converted functions given here from C++ to GoLang
+// https://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
 
 type Tree struct {
 	Left  *Tree
@@ -23,6 +24,41 @@ func insert(tree *Tree, value int) *Tree {
 	}
 	return tree
 }
+
+func findPath(tree *Tree,path *[]int, key int)  bool {
+	if(tree == nil){
+		return false;
+	}
+	*path = append(*path,tree.Value)
+	if tree.Value == key{
+		return true;
+	}
+	if (findPath(tree.Left,path,key))||
+	(findPath(tree.Right,path,key)){
+		return true;
+	}
+	n := len(*path) - 1
+	*path =(*path)[:n]
+	return false
+}
+
+func findLCA(tree *Tree, keyOne int, keyTwo int) int{
+	var keyOnePath [] int
+	var keyTwoPath [] int
+
+	if(!findPath(tree,&keyOnePath,keyOne)||!findPath(tree,&keyTwoPath,keyTwo)){
+		return -1
+	}
+	var i int
+	for i=0;i<len(keyOnePath) && i<len(keyTwoPath);i++{
+		if(keyOnePath[i]!=keyTwoPath[i]){
+			break
+		}
+	}
+	return keyOnePath[i-1]
+
+}
+
 
 func Hello() string {
 	return "Hello";
